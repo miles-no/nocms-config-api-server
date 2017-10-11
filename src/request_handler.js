9cbc9b888f24b52670abb8249718fa5867551807
Jon Arild Tørresdal <jon@torresdal.net>
2017-10-11 21:06:24 +0200

    fix: wrong lookup for environment config data - in src not just lib

--- src/request_handler.js
+++ src/request_handler.js
@@ -1,23 +1,23 @@
 const events = require('nocms-events');
 
-module.exports = (config) => {
+module.exports = (setup) => {
   return (req, res) => {
     const client = req.query.client;
     if (!client) {
       throw { status: 400, message: '400 Bad request. client missing in query string', response: '400 Bad request' };
     }
 
-    const adapter = config.adapters[client];
+    const adapter = setup.adapters[client];
     if (!adapter) {
       throw { status: 500, message: `Adapter missing for "${client}"`, response: '500 Internal server error' };
     }
 
-    const environmentConfig = config[config.environment];
+    const environmentConfig = setup.data[setup.environment];
     if (!environmentConfig) {
-      events.trigger('warning', `Config does not exist for environment ${config.environment}. Using default`);
+      events.trigger('warning', `Config does not exist for environment ${setup.environment}. Using default`);
     }
 
-    const configData = Object.assign(config.data.default, environmentConfig || {});
+    const configData = Object.assign(setup.data.default, environmentConfig || {});
 
     const clientConfig = adapter(configData);
 
